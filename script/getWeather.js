04df57b7cb316edfad7e8a35bae3add73a76f855
Dasha Mitreykina <d.mitreykina@mail.ru>
2025-09-21 15:44:18 +0300

    Fix rain icon svg

--- script/getWeather.js
+++ script/getWeather.js
@@ -40,7 +40,7 @@ const weatherCodeDescriptions = {
 };
 
 export function getWeatherIconPath(code) {
-    if ([0, 1].includes(code)) return "./svg/Sunny.svg";
+    if ([0, 1].includes(code)) return './svg/Sunny.svg';
     if ([2, 3].includes(code)) return './svg/Cloudy.svg';
     if ([45, 48].includes(code)) return './svg/Fog.svg';
     if ([51, 53, 55, 56, 57, 61, 63, 65, 66, 67, 80, 81, 82].includes(code)) return './svg/Rain.svg';
