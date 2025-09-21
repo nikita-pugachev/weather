af14b0836d426601055b4d343040e5334ee2f478
Dasha Mitreykina <d.mitreykina@mail.ru>
2025-09-21 15:59:14 +0300

    Fix all icons way

--- script/getWeather.js
+++ script/getWeather.js
@@ -40,12 +40,12 @@ const weatherCodeDescriptions = {
 };
 
 export function getWeatherIconPath(code) {
-    if ([0, 1].includes(code)) return './svg/Sunny.svg';
-    if ([2, 3].includes(code)) return './svg/Cloudy.svg';
-    if ([45, 48].includes(code)) return './svg/Fog.svg';
-    if ([51, 53, 55, 56, 57, 61, 63, 65, 66, 67, 80, 81, 82].includes(code)) return '../svg/Rain.svg';
-    if ([71, 73, 75, 77, 85, 86].includes(code)) return './svg/Snow.svg';
-    if ([95, 96, 99].includes(code)) return './svg/surface1.svg';
+    if ([0, 1].includes(code)) return 'svg/Sunny.svg';
+    if ([2, 3].includes(code)) return 'svg/Cloudy.svg';
+    if ([45, 48].includes(code)) return 'svg/Fog.svg';
+    if ([51, 53, 55, 56, 57, 61, 63, 65, 66, 67, 80, 81, 82].includes(code)) return 'svg/Rain.svg';
+    if ([71, 73, 75, 77, 85, 86].includes(code)) return 'svg/Snow.svg';
+    if ([95, 96, 99].includes(code)) return 'svg/surface1.svg';
 }
 
 export async function getWeather() {
